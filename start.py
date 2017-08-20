8d5527a7a5e0830493801f8fad71b9966edbc054
Cheng-Yu Pai <mail4charliepai@gmail.com>
2017-08-20 00:38:59 -0700

    counter

--- start.py
+++ start.py
@@ -15,7 +15,9 @@ def get_words(filename):
     shuffle(cards)
     return cards
 
-def render_card(card, font, bg):
+def render_card(card, counter, font, bg):
+    bg.fill((250, 250, 250))
+
     centerx = bg.get_rect().centerx
     word = font.render(card[0], 1, (10, 10, 10))
     wordpos = word.get_rect()
@@ -34,9 +36,17 @@ def render_card(card, font, bg):
         bg.blit(taboo_text, taboo_pos)
         centery += 40
 
+    counter_text = font.render(str(counter), 1, (250, 10, 10))
+    counter_pos = counter_text.get_rect()
+    counter_pos.centerx = 16
+    counter_pos.centery = 300 - 16
+    bg.blit(counter_text, counter_pos)
+
 def main():
     cards = get_words("words.txt")
     current_card = cards.pop(0)
+
+    counter = 0
 
     pygame.init()
     screen = pygame.display.set_mode((200, 300))
@@ -44,10 +54,9 @@ def main():
 
     bg = pygame.Surface(screen.get_size())
     bg = bg.convert()
-    bg.fill((250, 250, 250))
 
     font = pygame.font.Font(None, 36)
-    render_card(current_card, font, bg)
+    render_card(current_card, counter, font, bg)
 
     screen.blit(bg, (0, 0))
     pygame.display.flip()
@@ -56,15 +65,18 @@ def main():
         for event in pygame.event.get():
             if event.type == QUIT:
                 return
-            if event.type == KEYDOWN and event.key == K_ESCAPE:
+            elif event.type == KEYDOWN and event.key == K_ESCAPE:
                 return
-            if event.type == KEYDOWN and event.key == K_RETURN:
+            elif event.type == KEYDOWN and event.key == K_BACKSPACE:
+                counter = 0
+                render_card(current_card, counter, font, bg)
+            elif event.type == KEYDOWN and event.key == K_RETURN:
+                counter += 1
                 if len(cards) > 0:
                     current_card = cards.pop(0)
                 else:
                     current_card = ("Finished!", [])
-                bg.fill((250, 250, 250))
-                render_card(current_card, font, bg)
+                render_card(current_card, counter, font, bg)
         screen.blit(bg, (0, 0))
         pygame.display.flip()
 
