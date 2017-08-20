561341de116ceb1a93ea21ee0e69ce0fcfd6e96b
Cheng-Yu Pai <mail4charliepai@gmail.com>
2017-08-20 00:13:19 -0700

    read cards

--- start.py
+++ start.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
 
+from random import shuffle
 import pygame
 from pygame.locals import *
 
+def parse_card(line):
+    words = line.split(',')
+    return (words[0], words[1:])
+
+def get_words(filename):
+    with open(filename, 'r') as f:
+        content = f.readlines()
+    cards = [parse_card(line.strip()) for line in content]
+    shuffle(cards)
+    return cards
+
 def main():
+    cards = get_words("words.txt")
+
     pygame.init()
     screen = pygame.display.set_mode((200, 300))
     pygame.display.set_caption("Taboo")
@@ -25,7 +39,8 @@ def main():
         for event in pygame.event.get():
             if event.type == QUIT:
                 return
-
+            if event.type == KEYDOWN and event.key == K_ESCAPE:
+                return
         screen.blit(bg, (0, 0))
         pygame.display.flip()
 
